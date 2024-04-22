8b54b07a50c4f9f0b4434d6091e0f4a778cb5549
wheatfox <enkerewpo@hotmail.com>
2024-04-22 12:25:04 +0800

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
-rm-rf sdcard
+rm -rf sdcard
 mkdir sdcard
+
 make lmbench
+# make unixbench
+
+# npucore_testbench_path = ../../c_src/guest_os/npucore/NPUCore/NPUCore-3A5000-main/user/busybox_lua_testsuits/loongarch64/loongvisor
+npucore_testbench_path=../../c_src/guest_os/npucore/NPUcore-3A5000-main/user/busybox_lua_testsuites/loongarch64/loongvisor
+echo "npucore_testbench_path: $npucore_testbench_path"
+rm -rf $npucore_testbench_path/*
+
+# copy all files in sdcard to npucore_testbench_path
+cp -r sdcard/* $npucore_testbench_path
+echo "copy sdcard to npucore_testbench_path done"
